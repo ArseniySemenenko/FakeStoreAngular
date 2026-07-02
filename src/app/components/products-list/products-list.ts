d9f9a8dd155700dc13f8bef5a5c0fba513456731
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-07-02 11:01:17 +0300

    feat(products-list.ts, side-bar.html, side-bar.ts): side bar functionality
    sort by query parameters
    
    sort= "asc" | "desc" | "alph-up" | "alph-down"
    
    from = number
    to = number

--- src/app/components/products-list/products-list.ts
+++ src/app/components/products-list/products-list.ts
@@ -14,14 +14,57 @@ export class ProductsList implements OnInit{
   private readonly productsService = inject(ProductService);
 
   products = signal<IProduct[]>([]);
+
+  filter = input<string>("");
+  sort = input<string>("");
+  from = input<number>(0);
+  to = input<number>(1000);
+
   display = linkedSignal<IProduct[]>(() => {
-    if(!this.filter()) return this.products();
+    /*if(!this.filter()) return this.products();
     return this.products().filter(product => {
       if(this.filter() === "men") return product.category === "men's clothing"
       return product.category.includes(this.filter())
-    });
+    });*/
+
+    let products = this.products();
+
+    if(this.filter()){
+      if(this.filter() === "men"){
+        products = products.filter(p => p.category === "men's clothing");
+      }
+      products = products.filter(p => p.category.includes(this.filter()));
+    }
+
+    if(this.sort()){
+      console.log(this.sort());
+      switch(this.sort()){
+        case "asc":
+          products = products.sort((a, b) => a.price - b.price);
+          break;
+        case "desc":
+          products = products.sort((a, b) => b.price - a.price)
+          break;
+        case "alph-up":
+          products = products.sort((a, b) => a.title.localeCompare(b.title));
+          break;
+        case "alph-down":
+          products = products.sort((a, b) => b.title.localeCompare(a.title));
+      }
+    }
+
+    if(this.from()){
+      console.log(this.from())
+      products = products.filter(p => p.price >= this.from());
+    }
+
+    if(this.to()){
+      console.log(this.to())
+      products = products.filter(p => p.price <= this.to());
+    }
+
+    return products;
   })
-  filter = input<string>("");
 
   ngOnInit(){
     this.productsService.getProdutcs()
--- src/app/components/side-bar/side-bar.ts
+++ src/app/components/side-bar/side-bar.ts
@@ -1,14 +1,17 @@
-import { Component , model, signal } from '@angular/core';
+import { Component , model, signal} from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterLink],
+  imports: [RouterLink , FormsModule],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css',
 })
 export class SideBar {
   isShow = model<boolean>();
+
   priceFrom = signal(0);
   priceTo = signal(1000);
+
+  sortBy = signal("");
 }

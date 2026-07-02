7a5f4543b215311ec53b2438e3866ba620037394
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-07-02 13:37:52 +0300

    feat: create about page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ModalCart } from './components/modal-cart/modal-cart';
 import { ProductsList } from './components/products-list/products-list';
 import { ProductDetails } from './components/product-details/product-details';
 import { UserLogin } from './components/user-login/user-login';
+import { AboutPage } from './components/about-page/about-page';
 
 export const routes: Routes = [
     {path: "" , redirectTo: "catalog" , pathMatch: "full"},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     {path: 'cart' , component: ModalCart},
     {path: 'product/:id' , component: ProductDetails},
     {path: 'user' , component: UserLogin},
+    {path: 'about' , component: AboutPage},
 ];
